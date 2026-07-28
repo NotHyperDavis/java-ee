2d9c78b2985cc07dcac25a97d2dcc3a5355e8424
David Ferreira <ferreira.david9@hotmail.com>
2026-07-28 15:18:29 +0100

    terceiro commit

--- AleinAlert.java
+++ AleinAlert.java
@@ -22,12 +22,6 @@ public class AleinAlert {
         String json=response.body().toLowerCase();
 
         boolean isMorto = json.contains("\"status\":\"dead\"");
-
-
-        if (isMorto) {
-
-        }
-        // Especie Alien e Morto
         boolean isAlien = json.contains("\"species\":\"alien\"");
 
         if (isAlien && isMorto) {
